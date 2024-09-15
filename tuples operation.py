e0af075a03322ad942cf7014d10aaf366692776d
Tabid Hossain <tabid.hossain2009@gmail.com>
2024-09-15 20:58:10 +0600

    first commit

---
+++ tuples operation.py
@@ -0,0 +1,12 @@
+tup= ("tuple",False,3.2,1)
+print(tup)
+print(tup[1])
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+tuplex = tuplex + (420,)
+print(tuplex)
+
+tuple1 = (50,10,60,70,50,50,50)
+print(tuple1.count(50))
