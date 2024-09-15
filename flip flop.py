00f83c585dc41df48dcc39243caaecc85a7f016c
Tabid Hossain <tabid.hossain2009@gmail.com>
2024-09-15 21:16:17 +0600

    Danish bhai

---
+++ flip flop.py
@@ -0,0 +1,21 @@
+def paling(r):
+    end = len(r)-1
+    start = 0
+    
+    while(start< end):
+        
+        if (r[start] != r[end]):
+            return False
+        
+        start = start + 1
+        end = end - 1
+        
+    return True
+
+r = (1,2,3,3,2,1)
+
+if (paling(r)):
+    print("The tuple is flip-flop")
+    
+else:
+    print("The tuple is not flip flop")
